5ceee0be6a9db287c037194b064bb4bca01670b4
divyesh <divyesh.i.r75@gmail.com>
2025-08-08 00:18:33 +0530

    add cors middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res, next) => {
   res.status(200).json({
     status: "OK",
