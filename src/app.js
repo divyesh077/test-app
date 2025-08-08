5727200c3f94a4e475da3af1af01fa42599e9507
divyesh <divyesh.i.r75@gmail.com>
2025-08-08 21:09:09 +0530

    add helmet middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const helmet = require("helmet");
 const cors = require("cors");
+
 const morgan = require("morgan");
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 
 app.use(morgan("dev"));
