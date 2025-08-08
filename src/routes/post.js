8e07d805deac3c9f7aec299be3809da98eb20b3e
divyesh <divyesh.i.r75@gmail.com>
2025-08-08 22:27:54 +0530

    add get post by id routes

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,22 +1,31 @@
 const express = require("express");
 
 const router = express.Router();
-
+const posts = [
+  {
+    _id: "1",
+    title: "Hello World!!",
+  },
+  {
+    _id: "2",
+    title: "Welcome to tech!!!",
+  },
+];
 router.get("/", (req, res, next) => {
-  const posts = [
-    {
-      _id: "1",
-      title: "Hello World!!",
-    },
-    {
-      _id: "2",
-      title: "Welcome to tech!!!",
-    },
-  ];
   res.status(200).json({
     status: "OK",
     message: "Get posts successfully!!!",
     data: posts,
+  });
+});
+
+router.get("/:postId", (req, res, next) => {
+  const { postId } = req.params;
+  const post = posts.filter((post) => post._id === postId);
+  res.status(200).json({
+    status: "OK",
+    message: `Get post with postId ${postId}`,
+    data: post,
   });
 });
 
